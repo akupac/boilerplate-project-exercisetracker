3535a4ba2c7ef1898f270835a3420c612e6a5965
Alexandre A Kupac <akupac@gmail.com>
2024-07-26 13:32:59 +0200

    Implementações na inicialização do projeto: imports de libs, conexão com banco de dados e TODOs

--- index.js
+++ index.js
@@ -2,19 +2,46 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
 let userModel = require('./userModel');
 let exerciseModel = require('./exerciseModel');
 let logModel = require('./logModel');
 
+mongoose.connect(process.env.MONGO_URI);
 
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+/* You should provide your own project, not the example URL.
+# TODO:You can POST to /api/users with form data username to create a new user.
+# TODO:The returned response from POST /api/users with form data username will be an object with username and _id properties.
+# TODO:You can make a GET request to /api/users to get a list of all users.
+# TODO:The GET request to /api/users returns an array.
+# TODO:Each element in the array returned from GET /api/users is an object literal containing a user's username and _id.
+*/
 
+/*
+# TODO:You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
+# TODO:The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
+*/
+
+/*
+# TODO:You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
+# TODO:A request to a user's log GET /api/users/:_id/logs returns a user object with a count property representing the number of exercises that belong to that user.
+# TODO:A GET request to /api/users/:_id/logs will return the user object with a log array of all the exercises added.
+# TODO:Each item in the log array that is returned from GET /api/users/:_id/logs is an object that should have a description, duration, and date properties.
+# TODO:The description property of any object in the log array that is returned from GET /api/users/:_id/logs should be a string.
+# TODO:The duration property of any object in the log array that is returned from GET /api/users/:_id/logs should be a number.
+# TODO:The date property of any object in the log array that is returned from GET /api/users/:_id/logs should be a string. Use the dateString format of the Date API.
+# TODO:You can add from, to and limit parameters to a GET /api/users/:_id/logs request to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. limit is an integer of how many logs to send back.
+*/
 
 
 
